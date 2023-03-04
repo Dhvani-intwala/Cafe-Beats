447999fb7383433356dc65f18dabee2dcac7d646
Dhvani-intwala <intdhvani2627@gmail.com>
2023-03-04 14:09:24 +0000

    add get_user_details , display_menu_list and tabulate_data functions

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 """
 Import gspread to access and update data in our spreadsheet
 """
-
+import os
+from time import sleep
+import random
 import gspread
 import pyfiglet
 import pyfiglet.fonts
 from google.oauth2.service_account import Credentials
+from tabulate import tabulate
 from termcolor import colored
 
 SCOPE = [
@@ -24,23 +27,130 @@ SHEET = GSPREAD_CLIENT.open('Cafe Beats')
 MENU = SHEET.worksheet("menu")
 ORDER_LIST = SHEET.worksheet("order_list")
 
-data = MENU.get_all_values()
+MAX_MENU_ITEM = len(MENU.get_all_values()) - 2
+
+
+# Global variables
+user_data = []  # Contains user name, user order id, order type and address
 
 WELCOME_MSG = """
-Welcome to The Pizza Hub
+Welcome to The Cafe Beats
 Do you want to start your order now?
 [Y] - Yes
 [N] - No
 """
+ORDER_OPTION_MSG = """
+Enter your choice for order type -s
+[D] - For Home delivery
+[P] - For Pickup:
+"""
+DISPLAY_MENU_MSG = """
+[Item number] - To add item in your order list.
+[P] - To preview your order
+[Q] -  To quit
+"""
+
+
+def clear_screen():
+    """
+    Clears the console
+    """
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+
+
+def tabulate_data(user_info):
+    """
+    Formats the data in table form
+    """
+    format_data = tabulate(
+        user_info,
+        headers=["Item", "Name", "Cost"],
+        tablefmt="simple",
+        numalign="center",
+    )
+    print(format_data)
 
 
 def welcome():
     """
     Function to display home page
     """
-    title = "Welcome to the Cafe Beats"
+    title = "Welcome to The Cafe Beats"
     print(colored(pyfiglet.figlet_format(title, font="big"), "red"))
     print(colored(WELCOME_MSG, "green"))
+    while True:
+        start_order = input("\nEnter your choice:\n")
+        if start_order.capitalize() == "Y":
+            get_user_details()
+            break
+        elif start_order.capitalize() == "N":
+            print(colored("\nThanks for visiting us!\n", "yellow"))
+            break
+        else:
+            print(colored("Invaild input.Enter Y to start.\n", "red"))
+
+
+def get_user_details():
+    """
+    Gets user details like user name, order type, address and
+    appends them in user data list along with user order Id
+    """
+    user_data.clear()
+    user_name = input("Enter your name:\n")
+    user_data.append(user_name)
+    user_order_id = random.getrandbits(16)
+    user_data.append(user_order_id)
+    if user_name == "":
+        print(colored("\nEnter your name. Name is required\n", "red"))
+    else:
+        print(colored(f"\nWelcome {user_name}!\n", "yellow"))
+    while True:
+        delivery_type = input(ORDER_OPTION_MSG).capitalize()
+        if delivery_type not in ("D", "P"):
+            print(colored("\nInvalid delivery type. Try again.\n", "red"))
+            continue
+        if delivery_type == "D":
+            order_type = "Home delivery"
+            user_data.append(order_type)
+            print(
+                colored(f"Selected delivery type is: {order_type}\n", "yellow")
+            )
+            address = input("Enter your Address:\n")
+            if address == "":
+                print(
+                    colored("\nEnter your full address.\n", "red")
+                )
+            else:
+                print(
+                    colored(f"\nYour provided address is{address}\n", "yellow")
+                )
+            user_data.append(address)
+        elif delivery_type == "P":
+            order_type = "Pickup"
+            user_data.append(order_type)
+            print(
+                colored(f"\nSelected delivery type is: {order_type}", "yellow")
+                )
+            user_data.append("The Cafe Beats")
+
+        print(colored("\nLoading menu...", "green"))
+        sleep(5)
+        clear_screen()
+        display_menu_list()
+        break
+
+
+def display_menu_list():
+    """
+    Fetches the cafe beats menu from google sheets worksheet 'menu' and
+    displays it in formatted table form to user.
+    """
+    display_menu = MENU.get_all_values()
+    print(tabulate(display_menu))
+    print(DISPLAY_MENU_MSG)
 
 
 welcome()
